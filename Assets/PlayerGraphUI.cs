8106ae2caab467dff39e1c50e6c1802e67d7c55d
ryan-seamonster <ryan@seamonster.co.za>
2016-01-30 00:44:14 +0200

    Added a link between playerbehaviour and the graph UI.

---
+++ Assets/PlayerGraphUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerGraphUI : MonoBehaviour
+{
+    public PlayerBehaviour PlayerBehaviour;
+
+    public RectTransform GumptionBar;
+    public RectTransform MannersBar;
+    public RectTransform BreedingBar;
+    public RectTransform BottleBar;
+
+    private float _maxSize = 250f;
+    public float LerpScale = 0.2f;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    if (GumptionBar != null)
+        {
+            _maxSize = GumptionBar.sizeDelta.x;
+	    }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+	    if (PlayerBehaviour != null)
+	    {
+	        UpdateBar(GumptionBar, PlayerBehaviour.gumption);
+	        UpdateBar(MannersBar, PlayerBehaviour.manners);
+	        UpdateBar(BreedingBar, PlayerBehaviour.breeding);
+	        UpdateBar(BottleBar, PlayerBehaviour.bottle);
+	    }
+    }
+
+    void UpdateBar(RectTransform bar, float value)
+    {
+        if (bar != null)
+        {
+            Vector2 vec = bar.sizeDelta;
+            vec.x = _maxSize * value;
+            bar.sizeDelta = Vector2.Lerp(bar.sizeDelta, vec, LerpScale);
+        }
+    }
+}
